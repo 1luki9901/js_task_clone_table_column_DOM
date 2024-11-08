6ca4fca4b1bdf5733067dbce8bbe46681661ede8
Łukasz <1luki9901@gmail.com>
2024-11-08 23:30:39 +0100

    make build

---
+++ index.0af22b21.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){for(var e=document.querySelector("table").rows,n=0;n<e.length;n++){var t=e[n].cells,o=t[1].cloneNode(!0);e[n].insertBefore(o,t[t.length])}});
+//# sourceMappingURL=index.0af22b21.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+"use strict";
+// write your code here
+document.addEventListener("DOMContentLoaded", ()=>{
+    const table = document.querySelector("table");
+    const rows = table.rows;
+    for(let i = 0; i < rows.length; i++){
+        const cells = rows[i].cells;
+        const secondColumnCell = cells[1].cloneNode(true);
+        rows[i].insertBefore(secondColumnCell, cells[cells.length]);
+    }
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
